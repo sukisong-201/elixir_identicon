d876ae4b8ce82f4d961f1ce7c775d95a63795bd5
Suki Song <seul.song@avocadomattres.com>
2020-06-16 11:08:43 -0700

    pp

--- identicon/mix.exs
+++ identicon/mix.exs
@@ -11,14 +11,12 @@ defmodule Identicon.MixProject do
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
