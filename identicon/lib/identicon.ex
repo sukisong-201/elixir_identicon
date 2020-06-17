ef46c194b325166520925d995e2f2ed8132586dd
sukisong-201 <seul.song@avocadomattres.com>
2020-06-17 15:36:41 -0700

    6.45

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -6,19 +6,20 @@ defmodule Identicon do
       |> build_grid
     end
 
-    def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
-      %Identicon.Image{image | color: {r, g, b}}
+    def mirror_row(row) do
+      [first, second | _tail] = row
+
+      row ++ [second, first]
     end
 
     def build_grid(%Identicon.Image{hex: hex} = image) do
       hex
       |> Enum.chunk_every(3)
+      |> Enum.map(&mirror_row/1)
     end
 
-    def mirror_row(row) do
-      [first, second | _tail] = row
-
-      row ++ [second, first]
+    def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+      %Identicon.Image{image | color: {r, g, b}}
     end
 
     def hash_input(input) do
